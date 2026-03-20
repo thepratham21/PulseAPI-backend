8e5b5405990dfc9fe513cb8b1f35360b93a8ad57
Prathmesh Shinde <prathmeshshinde021@gmail.com>
2026-03-20 09:04:31 +0530

    Added: API routes integration for handling API requests

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
+import apiRoutes from "./routes/apiRoutes"
 
 const app: Application = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/apis", apiRoutes);
 
 // Health Check Route
 app.get("/", (req: Request, res: Response) => {
